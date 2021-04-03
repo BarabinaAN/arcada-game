57898ac79cd7838a45a774fbd431555f998e8672
Natalia Barabina <barabash4ka@gmail.com>
2021-04-03 20:00:32 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // images
 import './images/any/any-cite.svg';
+import './images/any/background.png';
+import './images/any/ball.png';
+import './images/any/block.png';
+import './images/any/platform.png';
+
 import './images/icon/favicon.svg';
 
 // styles
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
-const test = 'pre-commit && husky test';
+const game = {
+   start() {
+      this.ctx = document.getElementById('arcada-canvas').getContext('2d');
+      const background = new Image();
+      background.src = './images/any/background.png';
 
-console.log(test);
+      // выполнить перерисовку на следующем кадре всех запланированые до него изображений
+      window.requestAnimationFrame(() => {
+         this.ctx.drawImage(background, 0, 0); // запланированое изображение
+      });
+
+      console.log(background);
+   },
+};
+
+window.addEventListener('load', () => {
+   game.start();
+});
